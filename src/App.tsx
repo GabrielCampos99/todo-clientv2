01247f96bd662e8378d85e8077008bc8f130ca2f
Gabriel Campos <gabrielsouza.ccampos@gmail.com>
2023-07-02 20:39:19 -0300

    add dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-function App() {
+import { useState } from "react"
+import useDarkSide from "./hooks/useToggleThemeMode"
 
+function App() {
+  const { colorTheme, setTheme } = useDarkSide()
+
+  const [darkSide, setDarkSide] = useState(colorTheme === "light" ? true : false)
+
+  const toggleDarkMode = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const checked = event.target.checked
+    setTheme(colorTheme)
+    setDarkSide(checked)
+  }
   return (
     <div className="w-full flex justify-center">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <input type="checkbox" onChange={(e) => toggleDarkMode(e)} checked={darkSide} />
+      <h1 className="text-3xl font-bold underline dark:text-red-900">Hello world!</h1>
     </div>
   )
 }
