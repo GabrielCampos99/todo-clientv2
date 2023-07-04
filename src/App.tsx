ecc94c685eda1bd6d47fb4a06e5d0d961d85d219
Gabriel Campos <gabrielsouza.ccampos@gmail.com>
2023-07-03 21:26:29 -0300

    teste commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
-import { useState } from "react"
-import useDarkSide from "./hooks/useToggleThemeMode"
+import { useState } from 'react';
+import useDarkSide from './hooks/useToggleThemeMode';
 
 function App() {
-  const { colorTheme, setTheme } = useDarkSide()
+  const { colorTheme, setTheme } = useDarkSide();
 
-  const [darkSide, setDarkSide] = useState(colorTheme === "light" ? true : false)
+  const [darkSide, setDarkSide] = useState(
+    colorTheme === 'light' ? true : false,
+  );
 
   const toggleDarkMode = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const checked = event.target.checked
-    setTheme(colorTheme)
-    setDarkSide(checked)
-  }
+    const checked = event.target.checked;
+    setTheme(colorTheme);
+    setDarkSide(checked);
+  };
   return (
     <div className="w-full flex justify-center">
-      <input type="checkbox" onChange={(e) => toggleDarkMode(e)} checked={darkSide} />
-      <h1 className="text-3xl font-bold underline dark:text-red-900">Hello world!</h1>
+      <input
+        type="checkbox"
+        onChange={(e) => toggleDarkMode(e)}
+        checked={darkSide}
+      />
+      <h1 className="text-3xl font-bold underline dark:text-red-900">
+        Hello world!
+      </h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
