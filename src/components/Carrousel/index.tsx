b8789d33c0e0200955e7dbc777f85dd596baf2d7
Gabriel Campos <gabrielsouza.ccampos@gmail.com>
2023-07-09 11:26:49 -0300

    feat: create first version of carrousel

---
+++ src/components/Carrousel/index.tsx
@@ -0,0 +1,46 @@
+import { useRef, ReactNode } from 'react';
+import Slider, { Settings } from 'react-slick';
+import Typography from '../Typography';
+import Button from '../Button';
+
+type CarrouselProps<T> = {
+  data: T[];
+  render: (item: T) => ReactNode;
+};
+
+const Carrousel = <T,>({ data, render }: CarrouselProps<T>) => {
+  const sliderRef = useRef<Slider | null>(null);
+
+  const gotoNext = () => {
+    sliderRef.current?.slickNext();
+  };
+
+  const gotoPrev = () => {
+    sliderRef.current?.slickPrev();
+  };
+  const settings: Settings = {
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    infinite: false,
+  };
+
+  return (
+    <div className="h-screen ">
+      <Slider {...settings} ref={sliderRef}>
+        {data.map(render)}
+      </Slider>
+      <div className="flex justify-between">
+        <Button onClick={gotoPrev}>
+          <Typography variant="p">Anterior</Typography>
+        </Button>
+        <Button onClick={gotoNext}>
+          <Typography variant="p">Próximo</Typography>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Carrousel;
