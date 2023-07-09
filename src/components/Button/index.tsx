98d8f769e712514304706577a3d2fc4c49d0ac86
Gabriel Campos <gabrielsouza.ccampos@gmail.com>
2023-07-09 11:25:52 -0300

    feat: add button size prop

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react';
 
+type ButtonSize = 'sm' | 'md' | 'lg' | 'xl';
 type BgColorButton = 'bg-indigo-400' | 'bg-transparent';
 type ButtonProps = {
   children?: ReactNode;
@@ -7,6 +8,7 @@ type ButtonProps = {
   bgColor?: BgColorButton;
   hasBorder?: boolean;
   disabled?: boolean;
+  size?: ButtonSize;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button = (props: ButtonProps) => {
@@ -17,19 +19,28 @@ const Button = (props: ButtonProps) => {
     hasBorder = true,
     fullWidth = false,
     disabled = false,
+    size = 'md',
     ...rest
   } = props;
 
+  const buttonSizeMap: Record<ButtonSize, string> = {
+    sm: 'px-2 py-1',
+    md: 'px-3 py-2',
+    lg: 'px-6 py-4',
+    xl: 'px-8 py-6',
+  };
+
+  const buttonSize = buttonSizeMap[size];
   const fullWidthStyles = fullWidth ? 'w-full' : '';
   const hasBorderButton = hasBorder ? 'border rounded' : '';
   const isDisableStyles = disabled
     ? 'opacity-50 cursor-not-allowed'
-    : 'transition-transform hover:scale-[101%]';
+    : 'transition-transform';
 
   return (
     <button
       type={type}
-      className={`px-6 py-4 text-white uppercase ${isDisableStyles} ${fullWidthStyles} ${bgColor} ${hasBorderButton}`}
+      className={`text-white uppercase ${isDisableStyles} ${fullWidthStyles} ${bgColor} ${hasBorderButton} ${buttonSize}`}
       {...rest}
     >
       {children}
