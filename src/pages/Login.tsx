709ff54ffc907529eb6b0378c5c212c555c95564
Gabriel Campos <gabrielsouza.ccampos@gmail.com>
2023-07-29 12:58:22 -0300

    feat: login with react hook form and zod validation

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,20 @@
 import { ChevronLeft } from 'lucide-react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 import Button from '../components/Button';
 import Typography from '../components/Typography';
 import LoginForm from '../ui/forms/LoginForm';
+import {
+  LoginSchema,
+  LoginSchemaProps,
+} from '../types/schemas/login/loginSchema';
 
 const Login = () => {
-  const a = '';
+  const { handleSubmit, register } = useForm<LoginSchemaProps>({
+    resolver: zodResolver(LoginSchema),
+  });
 
+  const onSubmit: SubmitHandler<LoginSchemaProps> = (data) => console.log(data);
   return (
     <div className="max-w-7xl m-auto p-4">
       <Button bgColor="bg-transparent" size="sm" to="/" hasBorder>
@@ -14,7 +23,7 @@ const Login = () => {
       <Typography variant="h1" className="font-bold text-white">
         Login
       </Typography>
-      <LoginForm />
+      <LoginForm onSubmit={handleSubmit(onSubmit)} register={register} />
     </div>
   );
 };
--- src/ui/forms/LoginForm/index.tsx
+++ src/ui/forms/LoginForm/index.tsx
@@ -1,27 +1,38 @@
+import { UseFormRegister } from 'react-hook-form';
 import Button from '../../../components/Button';
 import Input from '../../../components/Input';
+import { LoginSchemaProps } from '../../../types/schemas/login/loginSchema';
 
-const LoginForm = () => (
-  <form>
-    <Input
-      label="Nome de usuário"
-      type="text"
-      placeholder="Digite o seu nome de usuário"
-      containerClassName="mt-8"
-      required
-    />
-    <Input
-      label="Senha"
-      type="password"
-      placeholder="Digite a sua senha"
-      containerClassName="mt-8"
-      required
-    />
+type LoginFormProps = {
+  register: UseFormRegister<LoginSchemaProps>;
+} & React.FormHTMLAttributes<HTMLFormElement>;
 
-    <Button fullWidth className="mt-12" type="submit">
-      Confirmar
-    </Button>
-  </form>
-);
+const LoginForm = (props: LoginFormProps) => {
+  const { register, ...rest } = props;
+  return (
+    <form {...rest}>
+      <Input
+        label="Nome de usuário"
+        type="text"
+        placeholder="Digite o seu nome de usuário"
+        containerClassName="mt-8"
+        required
+        {...register('username')}
+      />
+      <Input
+        label="Senha"
+        type="password"
+        placeholder="Digite a sua senha"
+        containerClassName="mt-8"
+        required
+        {...register('password')}
+      />
+
+      <Button fullWidth className="mt-12" type="submit">
+        Confirmar
+      </Button>
+    </form>
+  );
+};
 
 export default LoginForm;
