895da68c88ae18d8d70e46cb52ccf386db325714
Gabriel Campos <gabrielsouza.ccampos@gmail.com>
2023-08-02 21:59:09 -0300

    feat: dynamic taskCard

--- src/components/TaskCard/TaskCard.stories.ts
+++ src/components/TaskCard/TaskCard.stories.ts
@@ -11,5 +11,9 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
-  args: {},
+  args: {
+    title: 'Tarefa 1',
+    description: 'Terminar a tarefa 1',
+    created_at: new Date(),
+  },
 };
--- src/components/TaskCard/index.tsx
+++ src/components/TaskCard/index.tsx
@@ -1,30 +1,49 @@
 import { Pencil, Trash2 } from 'lucide-react';
 import Typography from '../Typography';
 
-const TaskCard = () => {
-  const a = '';
+type TaskCardProps = {
+  title: string;
+  description?: string;
+  created_at: string | Date;
+};
+const TaskCard = (props: TaskCardProps) => {
+  const { title, created_at: createdAt, description } = props;
   return (
-    <div className="flex items-center text-white gap-4 px-2 bg-stone-700 rounded py-1">
-      <input type="checkbox" className="h-5 w-5 cursor-pointer" />
-      <div className="flex flex-col gap-1 line-clamp-1">
-        <Typography
-          variant="h4"
-          className="font-bold text-ellipsis overflow-hidden whitespace-nowrap"
-        >
-          Do Math Homework Do Math Homework Do Math Homework Math HomeworkMath
-          HomeworkMath HomeworkMath HomeworkMath Homework
-        </Typography>
-        <Typography variant="p">Today At 16:45</Typography>
-      </div>
-      <div className="flex gap-1">
-        <Pencil
-          className="bg-neutral-800 rounded p-1 cursor-pointer hover:bg-neutral-500 transition"
-          size={32}
-        />
-        <Trash2
-          className="bg-neutral-800 rounded p-1 cursor-pointer hover:bg-neutral-500 transition"
-          size={32}
-        />
+    <div className="max-w-fit">
+      <div className="flex items-center text-white gap-4 px-2 bg-stone-700 rounded py-1">
+        <input type="checkbox" className="h-5 w-5 cursor-pointer" />
+        <div className="flex flex-col gap-1 line-clamp-1">
+          <Typography
+            variant="h4"
+            className="font-bold text-ellipsis overflow-hidden whitespace-nowrap"
+          >
+            {title}
+          </Typography>
+          {description && (
+            <Typography
+              variant="h4"
+              className="font-normal text-ellipsis overflow-hidden whitespace-nowrap"
+            >
+              {description}
+            </Typography>
+          )}
+
+          <Typography variant="p">
+            {`${new Intl.DateTimeFormat('pt-BR', {
+              dateStyle: 'full',
+            }).format(new Date(createdAt))}`}
+          </Typography>
+        </div>
+        <div className="flex gap-1">
+          <Pencil
+            className="bg-neutral-800 rounded p-1 cursor-pointer hover:bg-neutral-500 transition"
+            size={32}
+          />
+          <Trash2
+            className="bg-neutral-800 rounded p-1 cursor-pointer hover:bg-neutral-500 transition"
+            size={32}
+          />
+        </div>
       </div>
     </div>
   );
